1f2c8e8a57e1811fd44393029d4cc09c18346603
mabus <m.buscaroli@outlook.com>
2022-05-05 20:44:40 +0100

    completed

---
+++ README.md
@@ -0,0 +1,40 @@
+# TAMAGOTCHI - console minigame
+
+Simulate a tamagotchi game.
+Look after a pet in this mini game for the CLI 🖥
+
+## Futureproof Bootcamp
+
+#### Auguste Cohort - Lap1 - week1
+
+#### Learning OOP JS - ES6 Classes
+
+## Objective
+
+### Create a text-based game in nodejs that enables us to control a pet via user input.
+
+- Create a new class named Pet. A Pet should have at least three features name, happiness level, hunger level and any more you think are important.
+- Create a function that asks the user for their input. This function should cope with the three following occasions:
+- Creating a new pet and giving it a name and any other features defined.
+- User wants to feed or play with their pet..
+- User wants to check in with their pet and see how it’s happiness and hunger levels are.
+  Note: These will require you to create methods within your Pet class.
+  It is your choice as to whether you encapsulate your app logic in another class
+  Implement Error handling
+  How can you make this a more engaging game? What else would be cool for your pet to do? Can the user choose the type of food they feed their pet, with the pet randomly deciding if they like it or not?
+  To gather user input, the inquirer or prompt-sync modules are two of many alternatives to the builtin readline module. It is up to you what tool you use.
+
+## Technologies
+
+- NodeJS
+- prompt-sync
+
+## How to use
+
+```
+npm start
+```
+
+### Thanks
+
+Thank to [William](https://github.com/Izgardon) for brainstorming part of the game with me 👍
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,40 +1,164 @@
-class Tamagotchi {
+var prompt = require('prompt-sync')()
+
+class Pet {
   constructor(name, happiness = 5, hunger = 5) {
     this.name = name
     this.happiness = happiness
     this.hunger = hunger
+    this.isDead = false
+    this.userInput
+    this.foods = {
+      biscuits: 'loves',
+      meat: 'is crazy for',
+      cucumbers: "can't stand",
+    }
   }
 
-  // TODO add types of food
   feed() {
-    console.log('feeding time')
-    this.happiness++
-    this.hunger--
+    console.clear()
+    if (this.isDead) {
+      console.log('Your pet is dead')
+    }
+    console.log(`You are feeding ${this.name}`)
+    this.randomFoodReaction()
     console.log(
-      `${this.name}: happiness: ${this.happiness}, hunger: ${this.hunger}.`
+      `${this.name}: happiness: ${this.happiness}, hunger: ${this.hunger}.\n`
     )
   }
 
   play() {
-    console.log('feeding time')
+    console.clear()
+    console.log(`You are playing with ${this.name}`)
     this.happiness++
     this.hunger++
     console.log(
-      `${this.name}: happiness: ${this.happiness}, hunger: ${this.hunger}.`
+      `${this.name}: happiness: ${this.happiness}, hunger: ${this.hunger}.\n`
     )
+
+    if (this.hunger >= 10) {
+      this.isDead = true
+    }
   }
 
-  status() {
+  pullSledge() {
+    console.clear()
+    console.log(`${this.name} is pulling very hard!`)
+    this.happiness--
+    this.hunger = this.hunger + 2
     console.log(
-      `${this.name}: happiness: ${this.happiness}, hunger: ${this.hunger}.`
+      `${this.name}: happiness: ${this.happiness}, hunger: ${this.hunger}.\n`
     )
+
+    if (this.hunger >= 10) {
+      this.isDead = true
+    }
+  }
+
+  health() {
+    console.clear()
+    console.log('\n-------------')
+    console.log(
+      `${this.name}'s details:\n Happiness: ${this.happiness}\n Hunger: ${this.hunger}`
+    )
+    console.log('-------------\n')
+  }
+
+  petDied() {
+    console.clear()
+    console.log(console.log(`Sorry but you let ${this.name} die...`))
+    process.exit()
+  }
+
+  quit() {
+    console.clear()
+    console.log(`Come back soon or ${this.name} will get very sad!`)
+    process.exit()
+  }
+
+  displayOptions() {
+    console.log('What would you like to do?')
+    console.log('[f] -> Feed')
+    console.log('[p] -> Play')
+    console.log('[s] -> Pull the Sledge')
+    console.log('[h] -> Health details')
+    console.log('[q] -> Quit')
+  }
+
+  wrongInput() {
+    console.clear()
+    console.log(`\nWhat did you say?\n`)
+  }
+
+  // utility function called within this.feed()
+  randomFoodReaction() {
+    let foodChoiceNum = Math.floor(
+      Math.random() * Object.keys(this.foods).length
+    )
+    console.log(this.foods.length)
+    console.log('foodChoiceNum', foodChoiceNum)
+    let foodChoiceStr = Object.keys(this.foods)[foodChoiceNum]
+    console.log('foodChoiceStr', foodChoiceStr)
+
+    console.clear()
+    console.log(`${this.name} ${this.foods[foodChoiceStr]} ${foodChoiceStr}!`)
+    switch (foodChoiceNum) {
+      case 0:
+      case 1:
+        this.hunger--
+        this.happiness++
+        break
+      case 2:
+        this.happiness--
+    }
+  }
+
+  // game loop
+  loop() {
+    console.clear()
+    console.log(`${this.name} is happy to see you!\n`)
+
+    do {
+      if (this.isDead) this.petDied()
+
+      this.userInput = this.displayOptions()
+
+      this.userInput = prompt()
+
+      switch (this.userInput) {
+        case 'f':
+        case 'F':
+          this.feed()
+          break
+        case 'p':
+        case 'P':
+          this.play()
+          break
+        case 's':
+        case 'S':
+          this.pullSledge()
+          break
+        case 'h':
+        case 'H':
+          this.health()
+          break
+        case 'q':
+        case 'Q':
+          this.quit()
+          break
+        default:
+          this.wrongInput()
+          break
+      }
+    } while (this.userInput !== 'q' || this.userInput != 'Q' || this.isDead)
   }
 }
 
-function createTamagotchi(name) {
-  return new Tamagotchi(name)
-}
+// Start - ask for the pet's name until a non empty string is entered
+let petName
+do {
+  console.clear()
+  petName = prompt("What is your pet's name?  ")
+} while (petName === '')
 
-let zelda = createTamagotchi('Zelda')
-zelda.feed()
-zelda.play()
+let newGame = new Pet(petName)
+newGame.loop()
